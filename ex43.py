347469e25c2eb968f1f629028289689dc1b09fd3
LouisYang2019 <luoisyangis@hotmail.com>
2019-01-16 07:38:33 +0800

    add words

--- ex43.py
+++ ex43.py
@@ -41,8 +41,29 @@ class Death(Scene):
 class CentralCorridor(Scene):
     def enter(self):
         print(dedent("""
-        The Gothons of Planet Percal #25 have invaded your ship and 
+        The Gothons of Planet Percal #25 have invaded your ship and
+        destroyed your entire crew. You are the last surviving
+        member and your last mission is to get the neutron destruct
+        bomb from the Weapons Armory,put it in the bridge, and
+        blow the ship up after getting into an escape pod.
+
+        You're running down the central corridor to the Weapons
+        Armory when a Gothon jumps out, red scaly skin, dark grimy
+        teeth, and evil clown costume flowing around his hate
+        filled body. He's blocking the door to the Armory and
+        about to pull a weapon to blast you.
         """))
+
+        action = input('> ')
+        if action == 'shoot!':
+            print (dedent("""
+            Quick on the draw you yank out your blast and fire
+            it at the Gotton. His clown costume is flowing and
+            moving around his body, which throws off your aim.
+            Your laser hits his costume but misses him 
+            """))
+
+
         pass
 
 class LaserWeaponArmory(Scene):
