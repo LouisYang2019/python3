044e686c7ff0ddd0b43786d9d1a6fc4b722b1815
LouisYang2019 <luoisyangis@hotmail.com>
2019-01-21 07:30:40 +0800

    add code guess game

--- ex43.py
+++ ex43.py
@@ -57,21 +57,95 @@ class CentralCorridor(Scene):
         action = input('> ')
         if action == 'shoot!':
             print (dedent("""
-            Quick on the draw you yank out your blast and fire
-            it at the Gotton. His clown costume is flowing and
-            moving around his body, which throws off your aim.
-            Your laser hits his costume but misses him 
+                Quick on the draw you yank out your blast and fire
+                it at the Gotton. His clown costume is flowing and
+                moving around his body, which throws off your aim.
+                Your laser hits his costume but misses him entirely.
+                This completely ruins his brand new costume his mother
+                bought him, which makes him fly into an insane rage
+                and blast you repeatedly in the face until you are
+                dead.  Then he eats you.
             """))
+            return 'death'
 
-
-        pass
+        elif action == "dodge!":
+            print(dedent("""
+                Like a world class boxer you dodge, weave, slip and
+                slide right as the Gothon's blaster cranks a laser
+                past your head. In the middle of your artful dodge
+                your foot slips and you bang your head on the metal
+                wall and pass out. You wake up shortly after only to
+                die as the Gothon stomps on your head and eats you.
+                """))
+            return 'death'
+        elif action == 'tell a joke':
+            print(dedent("""
+            Lucky for you they made you learn Gothon insults in
+            the academy. You tell the one Gothon joke you know:
+            Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
+            fur fvgf nebhaq gur ubhfr. The Gothon stops, tries
+            not to laugh, then busts out laughing and can't move.
+            while he's laughing you run up and shoot him squre in
+            the head putting him down, then jump through the
+            Weapon Armory door.
+            """))
+            return 'laser_weapon_armory'
+        else:
+            print('DOES NOT COMPUTE!')
+            return 'central_corridor'
 
 class LaserWeaponArmory(Scene):
     def enter(self):
-        pass
+        print(dedent("""
+        You do a dive roll into the Weapon Armory, crouch and scan
+        the room for more Gothons that might be hiding.
+        There's a keypad lock on the box and you need the code to
+        get the bomb out. If you get the code wrong 10 times then
+        the lock closes forever and you can't get the bomb. The
+        code is 3 digits.
+        """))
+
+    code = f'{randint(1,9)}{randint(1,9)}{randint(1,9)}'
+    guess = input('[keypad]> ')
+    guesses = 0
+
+    while guess != code and guesses <10:
+        print('BZZZZZZZEDDDD!')
+        guesses += 1
+        guess = input('[keypad]> ')
+
+    if guess == code:
+        print (dedent("""
+        The container cliks open and the seal breaks, letting
+        gas out. You grab the neutron bomb and run as fast as
+        you can to the bridge where you must place it in the
+        right spot.
+        """))
+        return 'the_bridge'
+
+    else:
+        print(dedent("""
+        The lock buzzes one last time and then you hear a
+        ...
+        You decide to sit there, and  finally the Gothons
+        blow up the ship from their ship and you die.
+        """))
+        return 'death'
 
 class TheBridge(Scene):
     def enter(self):
+        print(dedent("""
+        You bursr onto the Bridge with the neutron destruct bomb
+        ...
+        weapons out yet, as they see the active bomb under your
+        arm and don't want to set it off.
+        """))
+
+        action = input('> ')
+
+        if action == 'throw the bomb':
+
+
         pass
 
 class EscapePod(Scene):
